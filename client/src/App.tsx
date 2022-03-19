1253df87a6ed0e3da551a98063d3b7fee8b5d86d
Miroslav I <styylzeh@gmail.com>
2022-03-19 23:57:44 +0200

    feat: hardcoded structure before styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,38 @@ import './App.css';
 
 function App() {
   return (
-   <><h1> Home Page</h1></>
+   <>
+    <div className='App'>
+      <div className='container'>
+        <div className='reservation__container'>
+          <div>
+            <h2 className='reservation__header'> Reservations</h2>
+            <div className='reservation__cards'>
+              <div className='reservation__card-container'>
+                <p> Name</p>
+              </div>
+            </div>
+            <div className='reservation__input-container'>
+              <input/>
+              <button>Add</button>
+            </div>
+          </div>
+          <div className='customer-food__container'>
+            <div className='customer-foods__card-container'>
+              <h3>Name</h3>
+              <div className='customer-foods__food'>
+                <p>Cake</p>
+              </div>
+              <div className='customer-foods__input-container'>
+                <input/>
+                <button>Add</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </>
   );
 }
 
