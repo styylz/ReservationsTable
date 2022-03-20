045a9ea65092a7850ff2567c7e550f5f865ea6e8
Miroslav I <styylzeh@gmail.com>
2022-03-21 01:46:54 +0200

    feat: created function which adds food

--- client/src/features/customerSlice.ts
+++ client/src/features/customerSlice.ts
@@ -10,6 +10,11 @@ interface Customer {
     food: string[]
 }
 
+interface AddFoodToCustomer {
+    id: string;
+    food: string;
+}
+
 const initialState: CustomerState = {
     value: []
 }
@@ -20,10 +25,17 @@ const customerSlice = createSlice({
     reducers: {
         addCustomer: (state, action: PayloadAction<Customer>) => {
             state.value.push(action.payload)
-        }
+        },
+        addFoodToCustomer: (state, action: PayloadAction<AddFoodToCustomer>) => {
+            state.value.forEach((customer) => {
+                if (customer.id === action.payload.id) {
+                    customer.food.push(action.payload.food)
+                }
+            })
+        },
     }
 });
 
-export const { addCustomer } = customerSlice.actions
+export const { addCustomer, addFoodToCustomer } = customerSlice.actions
 
 export default customerSlice.reducer
