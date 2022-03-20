5549ad10eaa1b4118c4b24c4febf07f7d07c2975
Miroslav I <styylzeh@gmail.com>
2022-03-21 01:58:35 +0200

    feat: food is now displayed dynamically on the card by user

--- client/src/components/CustomerCard.tsx
+++ client/src/components/CustomerCard.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addFoodToCustomer } from '../features/customerSlice';
 
 interface CustomerCardTypes {
     id: string;
@@ -6,6 +8,9 @@ interface CustomerCardTypes {
     food: string[]
   }
 function CustomerCard({id, name, food}: CustomerCardTypes) {
+
+  const [customerInput, setCustomerInput] = useState('')
+  const dispatch = useDispatch()
 
   return (
     <div className='customer-food__card-container'>
@@ -15,8 +20,22 @@ function CustomerCard({id, name, food}: CustomerCardTypes) {
         {food.map(x => <p>{x}</p>)}
       </div>
       <div className='customer-food__input-container'>
-        <input/>
-        <button>Add</button>
+        <input
+          value={customerInput}
+          onChange={(e) => setCustomerInput(e.target.value)}
+        />
+        <button
+          onClick={()=> {
+            if(!customerInput) return
+
+            dispatch(addFoodToCustomer({
+              id,
+              food: customerInput
+             })
+             )
+             setCustomerInput("")
+        }}
+        >Add</button>
       </div>
    </div>
   </div>
