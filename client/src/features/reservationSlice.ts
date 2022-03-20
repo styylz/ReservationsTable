bec4f87cdb3e3d6d54f4f52841ebbac457666f40
Miroslav I <styylzeh@gmail.com>
2022-03-20 21:25:35 +0200

    feat: init slice for reservations

---
+++ client/src/features/reservationSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    value: []
+}
+
+export const reservationSlice = createSlice({
+    name: "reservations",
+    initialState,
+    reducers: {}
+})
